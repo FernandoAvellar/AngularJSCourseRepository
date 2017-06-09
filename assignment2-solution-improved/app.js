3cdcf2a664a746f49c01918aacec6fdbc89c754d
Fernando Avellar <fernando.avellar@inatel.br>
2017-06-09 10:11:55 -0300

    Melhoria na identacao

--- assignment2-solution-improved/app.js
+++ assignment2-solution-improved/app.js
@@ -1,33 +1,33 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('ShoppingListCheckOff', [])
- .controller('ToBuyController', ToBuyController)
- .controller('AlreadyBoughtController', AlreadyBoughtController)
- .controller('NewItemController', NewItemController)
- .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+  angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .controller('NewItemController', NewItemController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
- ////////////////////////////////////////////////////////////////////////////
- // ToBuy Controller  //
- ////////////////////////////////////////////////////////////////////////////
- ToBuyController.$inject = ['ShoppingListCheckOffService'];
- function ToBuyController(ShoppingListCheckOffService) {
+  ////////////////////////////////////////////////////////////////////////////
+  // ToBuy Controller  //
+  ////////////////////////////////////////////////////////////////////////////
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
 
-  var buyList = this;
+    var buyList = this;
 
-  buyList.items = ShoppingListCheckOffService.getBuyList();
+    buyList.items = ShoppingListCheckOffService.getBuyList();
 
-  buyList.boughtButton = function (itemIndex) {
+    buyList.boughtButton = function (itemIndex) {
       ShoppingListCheckOffService.changeItemFromBuyListToBoughtList(itemIndex);
-  };
+    };
 
-  buyList.deleteButton = function (itemIndex) {
+    buyList.deleteButton = function (itemIndex) {
       ShoppingListCheckOffService.deleteItemFromBuyList(itemIndex);
-  };
+    };
 
-  buyList.editButton = function (itemIndex) {
+    buyList.editButton = function (itemIndex) {
       ShoppingListCheckOffService.returnSelectedItemToNewItemInput(itemIndex);
-  };
+    };
 
   }
 
@@ -35,20 +35,20 @@ angular.module('ShoppingListCheckOff', [])
   // AlreadyBought Controller  //
   ////////////////////////////////////////////////////////////////////////////
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
- function AlreadyBoughtController(ShoppingListCheckOffService) {
-  var boughtList = this;
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var boughtList = this;
 
-  boughtList.items = ShoppingListCheckOffService.getBoughtList();
+    boughtList.items = ShoppingListCheckOffService.getBoughtList();
 
-  boughtList.returnToBuyListButton = function (itemIndex) {
-    ShoppingListCheckOffService.returnSelectedItemToBuyList(itemIndex);
-  };
+    boughtList.returnToBuyListButton = function (itemIndex) {
+      ShoppingListCheckOffService.returnSelectedItemToBuyList(itemIndex);
+    };
 
   }
 
- ////////////////////////////////////////////////////////////////////////////
- // NewItem Controller //
- ////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////
+  // NewItem Controller //
+  ////////////////////////////////////////////////////////////////////////////
   NewItemController.$inject = ['ShoppingListCheckOffService', '$rootScope'];
   function NewItemController(ShoppingListCheckOffService, $rootScope) {
     var newItem = this;
@@ -56,11 +56,11 @@ angular.module('ShoppingListCheckOff', [])
     refreshInputFields();
 
     newItem.addButton = function () {
-    ShoppingListCheckOffService.addToBuyList(newItem.newItemName, newItem.newItemQuantity);
+      ShoppingListCheckOffService.addToBuyList(newItem.newItemName, newItem.newItemQuantity);
       clearNewItemView();
     };
 
-    $rootScope.$on('refreshInputFields', function() {
+    $rootScope.$on('refreshInputFields', function () {
       refreshInputFields();
     });
 
@@ -80,61 +80,61 @@ angular.module('ShoppingListCheckOff', [])
   ////////////////////////////////////////////////////////////////////////////
   // Service ShoppingListCheckOffService//
   ////////////////////////////////////////////////////////////////////////////
- function ShoppingListCheckOffService($rootScope) {
-  var service = this;
+  function ShoppingListCheckOffService($rootScope) {
+    var service = this;
 
-  var buyListItems =
-  [ { itemName : "bolacha maizena", itemQuantity : "10 pacotes" },
-    { itemName : "presunto", itemQuantity : "300 gramas" },
-    { itemName : "cafe", itemQuantity : "2 pacotes" },
-    { itemName : "sorvete", itemQuantity : "1 pote" },
-    { itemName : "pizza congelada", itemQuantity : "3 caixas" } ];
+    var buyListItems =
+      [{ itemName: "bolacha maizena", itemQuantity: "10 pacotes" },
+      { itemName: "presunto", itemQuantity: "300 gramas" },
+      { itemName: "cafe", itemQuantity: "2 pacotes" },
+      { itemName: "sorvete", itemQuantity: "1 pote" },
+      { itemName: "pizza congelada", itemQuantity: "3 caixas" }];
 
-  var boughtListItems = [];
+    var boughtListItems = [];
 
-  var newItemContent = {itemName : "", itemQuantity: ""};
+    var newItemContent = { itemName: "", itemQuantity: "" };
 
-  service.getBuyList = function () {
-    return buyListItems;
-  };
+    service.getBuyList = function () {
+      return buyListItems;
+    };
 
-  service.getBoughtList = function () {
-    return boughtListItems;
-  };
+    service.getBoughtList = function () {
+      return boughtListItems;
+    };
 
-  service.getNewItemContent = function () {
-    return newItemContent;
+    service.getNewItemContent = function () {
+      return newItemContent;
+    }
+
+    service.changeItemFromBuyListToBoughtList = function (itemIndex) {
+      var removedItem = buyListItems.splice(itemIndex, 1);
+      boughtListItems.push(removedItem[0]);
+    };
+
+    service.deleteItemFromBuyList = function (itemIndex) {
+      buyListItems.splice(itemIndex, 1);
+    }
+
+    service.addToBuyList = function (newItemName, newItemQuantity) {
+      var newItem =
+        {
+          itemName: newItemName,
+          itemQuantity: newItemQuantity
+        };
+      buyListItems.push(newItem);
+    }
+
+    service.returnSelectedItemToNewItemInput = function (itemIndex) {
+      var editItem = buyListItems.splice(itemIndex, 1)[0];
+      newItemContent = { itemName: editItem.itemName, itemQuantity: editItem.itemQuantity };
+      $rootScope.$broadcast('refreshInputFields');
+    }
+
+    service.returnSelectedItemToBuyList = function (itemIndex) {
+      var returnedItem = boughtListItems.splice(itemIndex, 1);
+      buyListItems.push(returnedItem[0]);
+    }
+
   }
-
-  service.changeItemFromBuyListToBoughtList = function (itemIndex) {
-    var removedItem = buyListItems.splice(itemIndex, 1);
-    boughtListItems.push(removedItem[0]);
-  };
-
-  service.deleteItemFromBuyList = function (itemIndex) {
-    buyListItems.splice(itemIndex, 1);
-  }
-
-  service.addToBuyList = function (newItemName, newItemQuantity) {
-    var newItem =
-      {
-        itemName : newItemName,
-        itemQuantity : newItemQuantity
-      };
-    buyListItems.push(newItem);
-  }
-
-  service.returnSelectedItemToNewItemInput = function (itemIndex) {
-    var editItem = buyListItems.splice(itemIndex, 1)[0];
-    newItemContent = {itemName : editItem.itemName, itemQuantity: editItem.itemQuantity};
-    $rootScope.$broadcast('refreshInputFields');
-  }
-
-  service.returnSelectedItemToBuyList = function (itemIndex) {
-    var returnedItem = boughtListItems.splice(itemIndex, 1);
-    buyListItems.push(returnedItem[0]);
-  }
-
-}
 
 })();
